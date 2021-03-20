d2c74784402f05ed62c7534850ea1a8b60f76426
Mauricio Badilla <mbadilla86@gmail.com>
2021-03-20 15:21:41 -0600

    heroku

---
+++ db/migrate/20210320043543_create_friends.rb
@@ -0,0 +1,12 @@
+class CreateFriends < ActiveRecord::Migration[6.1]
+  def change
+    create_table :friends do |t|
+      t.integer :user, null: false, foreign_key: true
+      t.integer :friend
+      t.string :tweet
+      t.integer :id
+
+      t.timestamps
+    end
+  end
+end
